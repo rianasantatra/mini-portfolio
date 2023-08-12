69aa970b80c7726429d7c02176d55496a9e07fbb
rianasantatra <rianasantatra665@gmail.com>
2023-08-12 22:55:37 +0300

    update css skills&projects - tabs function main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
-/*=============== FILTERS TABS ===============*/
+/*=============== FILTERS tabs ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tc) /** tc = tab content */ => {
+      tc.classList.remove("filters__active");
+    });
+
+    target.classList.add("filters__active");
+
+    tabs.forEach((t) => {
+      t.classList.remove("filter-tab-active");
+    });
+    tab.classList.add("filter-tab-active");
+  });
+});
 
 /*=============== DARK LIGHT THEME ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
