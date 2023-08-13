32a618458a8473e403d8222457ecf5351503cb9b
rianasantatra <rianasantatra665@gmail.com>
2023-08-13 14:17:59 +0300

    edit footer section & creation dark/light mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,5 +20,32 @@ tabs.forEach((tab) => {
 });
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "fa-sun";
 
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  document.body.classList.contains(iconTheme) ? "fa-moon" : "fa-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "fa-sun" ? "remove" : "add"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme);
+  localStorage.setItem("selected-icon", getCurrentIcon);
+});
 /*=============== SCROLL REVEAL ANIMATION ===============*/
