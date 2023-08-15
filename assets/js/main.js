504b180db63ba2322c038728229a6097c15ac2e4
rianasantatra <rianasantatra665@gmail.com>
2023-08-15 11:57:22 +0300

    dark theme icon issue solved/edit scrollreveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,15 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon);
 });
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: "2500",
+  delay: "400",
+});
+sr.reveal(".profile__border");
+sr.reveal(".profile__name", { delay: 500 });
+sr.reveal(".profile__border");
+sr.reveal(".profile__border");
+sr.reveal(".profile__border");
+sr.reveal(".profile__border");
